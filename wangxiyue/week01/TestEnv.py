9b6ad88ec9829cff8b7c6c871d44da093d3261b9
虫子 <wangxiyue.xy@163.com>
2025-02-27 14:45:08 +0800

    Update TestEnv.py

--- wangxiyue/week01/TestEnv.py
+++ wangxiyue/week01/TestEnv.py
@@ -7,3 +7,8 @@ print(torch.__version__)
 
 
 # mac book pro m1  个别版本冲突
+
+###########
+## 1 目前本地， 除window 外， 还有一个 开发用的  macbook m1   ， 现在版本的pytorch  和  mac版本 TensorFlow 冲突， 
+## 后续 linux 工作站用于训练
+#############
