849e34386f3bb3fd09db55c2ffb481cc9f127aff
虫子 <wangxiyue.xy@163.com>
2025-02-27 14:38:19 +0800

    Update TestEnv.py

--- wangxiyue/week01/TestEnv.py
+++ wangxiyue/week01/TestEnv.py
@@ -2,5 +2,8 @@ import torch
 #__version__:
 print(torch.__version__)
 
-# 按照加成安装环境需要 torch 版本与numpy 冲突， 需要重新那幢numpy
+# 按照加成安装环境需要 torch 版本与numpy 冲突， 需要重新安装numpy
 # pip install --upgrade --force-reinstall numpy
+
+
+# mac book pro m1  个别版本冲突
