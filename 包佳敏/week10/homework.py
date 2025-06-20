fb23285674d9dc4fdf06e930bd12bb7011c710ee
蛋挞 <baojiamin@dantadebijibendiannao.local>
2025-06-20 21:38:16 +0800

    add

--- 包佳敏/week10/homework.py
+++ 包佳敏/week10/homework.py
@@ -8,7 +8,7 @@ import kagglehub
 from transformers import pipeline
 #classifier = pipeline(
 #"sentiment-analysis",
-#model="uer/roberta-base-finetuned-dianping-chinese")
+#model="google-bert/bert-base-chinese")
 #classifier([
 #"店家发货很快，包装很严实，日期也很好，好评。",
 #"味道一般，不放冰箱很快就软化了。"])
@@ -18,26 +18,26 @@ import matplotlib.pyplot as plt
 import pickle
 #第一步：数据加载与预处理
 ds_comments = []
-#with open('/Users/baojiamin/Desktop/jd_comment_data.csv', 'r', newline='') as f:
-#    reader = csv.DictReader(f)
-#    for row in reader:
+with open('/Users/baojiamin/Desktop/jd_comment_data.csv', 'r', newline='') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
         #print(row['Comment'], row['Star'])#[0-5]
-#        if row['评分（总分5分）(score)'] == None: continue
-#        ds_comments.append((row['评价内容(content)'], row['评分（总分5分）(score)'])) # 0-1标签
+        if row['评分（总分5分）(score)'] == None: continue
+        ds_comments.append((row['评价内容(content)'], row['评分（总分5分）(score)'])) # 0-1标签
 
-#comments_len = [len(c) for c,v in ds_comments]
+comments_len = [len(c) for c,v in ds_comments]
 #plt.hist(comments_len, bins=50)
 #plt.xlabel('Length of comments')
 #plt.ylabel('Number of comments')
 #plt.title('Distribution of comments length')
 #plt.show()
 #plt.boxplot(comments_len) # 绘制箱线图
-#ds_comments = [values for values in ds_comments if len(values[0]) in range(5,150)] # 过滤掉空评论
-#with open('comments.pkl', 'wb') as f:
-#    pickle.dump(ds_comments, f)
+ds_comments = [values for values in ds_comments if len(values[0]) in range(5,150)] # 过滤掉空评论
+ds_comments = [values for values in ds_comments if len(values[0]) != '此用户未填写评价内容']
+with open('comments.pkl', 'wb') as f:
+    pickle.dump(ds_comments, f)
 with open('comments.pkl', 'rb') as f:
     ds_comments = pickle.load(f)
-
 import torch
 def build_collate(tokenizer):
     def collate_fn(batch):
@@ -45,18 +45,18 @@ def build_collate(tokenizer):
         # Tokenize the texts
         encodings = tokenizer(list(texts), truncation=True, padding=True, return_tensors='pt')
         # Convert labels to tensor
-        labels = torch.tensor([1 if int(label) > 3 else 0 for label in labels], dtype=torch.float)
+        labels = torch.tensor([(int(label)-1) for label in labels])  # Convert to 0-1 labels
         return encodings, labels
     return collate_fn
 
 from transformers import AutoTokenizer
-tokenizer = AutoTokenizer.from_pretrained("uer/roberta-base-finetuned-dianping-chinese")
+tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-chinese")
 
 from torch.utils.data import DataLoader, Dataset
-dl = DataLoader(ds_comments, batch_size=64, collate_fn=build_collate(tokenizer), shuffle=True)
+dl = DataLoader(ds_comments, batch_size=16, collate_fn=build_collate(tokenizer), shuffle=True)
 
 from transformers import AutoModelForSequenceClassification
-model = AutoModelForSequenceClassification.from_pretrained("uer/roberta-base-finetuned-dianping-chinese", num_labels=2)
+model = AutoModelForSequenceClassification.from_pretrained("google-bert/bert-base-chinese", num_labels=5)
 #model.bert.training = False
 
 import torch.optim as optim
@@ -81,18 +81,13 @@ for epoch in range(3):
 
             # 计算损失
             # CrossEntropyLoss需要将logits和targets展平
-            # logits: [batch_size, seq_len, vocab_size]
-            # targets: [batch_size, seq_len]
-            # 展平为 [batch_size * seq_len, vocab_size] 和 [batch_size * seq_len]
             logits = outputs.logits
             # logits: [batch_size, num_labels]
-            predictions = tf.math.softmax(outputs.logits.detach().numpy(), axis=-1)
+            #predictions = torch.softmax(outputs.logits, axis=-1)
             #print(predictions)
             # label: [batch_size]
-            reslut = tf.argmax(predictions, axis=-1)
-            #reslut = tf.expand_dims(reslut,axis=-1)
-           
-            loss = criterion(torch.tensor(reslut.numpy(), dtype=torch.float), label)
+            #reslut = torch.argmax(predictions, axis=-1)
+            loss = criterion(logits, label)
 
             # 反向传播
             optimizer.zero_grad()
@@ -104,4 +99,4 @@ for epoch in range(3):
             writer.add_scalar('Loss/train', loss.item(), train_loss_cnt)
             train_loss_cnt += 1
 
-torch.save(model.state_dict(), 'roberta-base-finetuned-dianping-chinese_state.bin')
+torch.save(model.state_dict(), 'google-bert-base-chinese_state.bin')
